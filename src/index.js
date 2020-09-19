761072980ebef17c2f6564d274b194510bbbd6d5
ECMaccoux <emaccoux@wisc.edu>
2020-09-19 11:16:30 -0500

    Added dashboard animations and buttons
    Added animations to the individual cards on the List View dashboard as well as buttons to access Graph View and All Journals

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Switch, Route, BrowserRouter } from "react-router-dom";
+
 import LandingPage from "./LandingPage";
 import TopNav from "./Components/TopNav";
-import { Router, Switch, Route, BrowserRouter } from "react-router-dom";
+
 import Register from "./authentication/Register";
 import Login from "./authentication/Login";
-import Dashboard from "./Dashboard"
+
+import ListView from "./Dashboard/ListView"
+import GraphView from "./Dashboard/GraphView"
+import JournalView from "./Dashboard/JournalView"
 
 class Main extends React.Component {
   constructor(props) {
@@ -20,13 +25,23 @@ class Main extends React.Component {
               <TopNav isLanding={false}></TopNav>
               <Register />
           </Route>
-          <Route path="/dashboard">
-              <TopNav isHome={true}></TopNav>
-              <Dashboard />
-          </Route>
           <Route path="/login">
               <Login />
           </Route>
+          
+          <Route path="/dashboard">
+              <TopNav isHome={true}></TopNav>
+              <ListView />
+          </Route>
+          <Route path="/dashboard/graph">
+              <TopNav isHome={true}></TopNav>
+              <GraphView />
+          </Route>
+          <Route path="/dashboard/journal">
+              <TopNav isHome={true}></TopNav>
+              <JournalView />
+          </Route>
+
           <Route exact path="/">
             <div>
               <TopNav isLanding={true}></TopNav>

4cbfd74cb725b3b3af46263d68c4447a49782db4
ECMaccoux <emaccoux@wisc.edu>
2020-09-19 10:29:29 -0500

    Updated dashboard layout
    Styles are currently on-hold (lots of white, very wow), but the layout is (mostly) there!

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import { Container, Row, Col, Form, Button, Card } from 'react-bootstrap'
 
 export default class Dashboard extends React.Component {
     constructor(props) {
@@ -8,7 +8,82 @@ export default class Dashboard extends React.Component {
 
     render() {
         return (
-            <p>test</p>
+            <div>
+                <Container fluid="md">
+                    <br></br>
+                    <Row>
+                        <h2>Hello, [NAME]!</h2>
+                    </Row>
+                    <br></br>
+                    <Row className="upos-dash-row">
+                        <Col>
+                            <Card>
+                                <Card.Body><h4>Daily Check-In</h4></Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row className="upos-dash-row">
+                        <Col>
+                            <Card>
+                                <Card.Body><h5>Anxiety</h5></Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card>
+                                <Card.Body><h5>Fitness</h5></Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row className="upos-dash-row">
+                        <Col>
+                            <Card>
+                                <Card.Body><h5>Depression</h5></Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card>
+                                <Card.Body><h5>Partner Relationships</h5></Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row className="upos-dash-row">
+                        <Col>
+                            <Card>
+                                <Card.Body><h5>Diet</h5></Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card>
+                                <Card.Body><h5>School</h5></Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row className="upos-dash-row">
+                        <Col>
+                            <Card>
+                                <Card.Body><h5>Water</h5></Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card>
+                                <Card.Body><h5>Work</h5></Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row className="upos-dash-row">
+                        <Col>
+                            <Card>
+                                <Card.Body><h5>Sleep</h5></Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card>
+                                <Card.Body><h5>Period Tracker</h5></Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         )
     }
 }

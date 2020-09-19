d346f7710873c89a87e091592681a8778e6bd2df
thok-patrol <rswerner@wisc.edu>
2020-09-19 02:11:32 -0500

    Added better way to serve files from express

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const path = require('path')
+const PATH = path.join(__dirname, "../dev/")
+
 
 const App = express()
 
-App.get('/', (req, res) => {
-    res.send('ok')
-})
+App.get("/*?", (req, res) => {
+    //console.log(CURR_PATH + '/img/' + req.params[0])
+    res.sendFile(PATH + req.params[0]);
+  });
 
 App.listen(3000, () => {
     console.log('Running on port 3000!')

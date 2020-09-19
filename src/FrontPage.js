151d67fe5deb858e519c3bef91808023b7d285c1
ECMaccoux <emaccoux@wisc.edu>
2020-09-19 02:17:11 -0500

    Fixed navbar, added jumbotron
    Added more "sample" React Bootstrap components, fixed miscellanious bugs with the navbar and CSS stylesheet

--- src/FrontPage.js
+++ src/FrontPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Navbar, Nav, Row, Col, Container } from "react-bootstrap";
+import { Button, Navbar, Nav, Row, Col, Container, Jumbotron } from "react-bootstrap";
 
 export default class FrontPage extends React.Component {
     constructor(props) {
@@ -9,8 +9,13 @@ export default class FrontPage extends React.Component {
     render() {
         return (
             <div>
-                <Container fluid id="home">
-
+                <Container fluid="md" id="home">
+                    <Jumbotron fluid>
+                        <Container style={{textAlign: "center"}}>
+                            <h1>Welcome to U+ ("you-positive")</h1>
+                            <p>It's health and fitness tracking, made for real people.</p>
+                        </Container>
+                    </Jumbotron>
                 </Container>
             </div>
         );
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -9,15 +9,15 @@ export default class TopNav extends React.Component {
     render() {
         return (
             <Navbar bg="light" sticky="top" expand="md">
-                <Nav className="container-fluid">
+                <Nav className="container-fluid" style={{paddingLeft: 0, paddingRight: 0}}>
                     <Nav.Item>
                         <Navbar.Brand href="#home">U+</Navbar.Brand>
                     </Nav.Item>
                     <Nav.Item className="ml-auto">
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav" className="ml-auto">
-                            <Button variant="primary">Sign Up</Button>
-                            <Button variant="primary">Log In</Button>
+                            <Button variant="primary" className="upos-nav-btn">Sign Up</Button>
+                            <Button variant="primary" className="upos-nav-btn">Log In</Button>
                         </Navbar.Collapse>
                     </Nav.Item>
                 </Nav>

171d8ecb92ab8f0cef8db36416814dbf273b39da
thok-patrol <rswerner@wisc.edu>
2020-09-19 01:17:05 -0500

    Reinstalled express? Added basic express server. Added nodemon.

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
-//future server here
+const express = require("express");
+
+const App = express()
+
+App.get('/', (req, res) => {
+    res.send('ok')
+})
+
+App.listen(3000, () => {
+    console.log('Running on port 3000!')
+})

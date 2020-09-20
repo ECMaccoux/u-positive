6429e89d2b9aa87c52f3387e0f7bd4cdf93e2f67
thok-patrol <rswerner@wisc.edu>
2020-09-20 08:00:29 -0500

    Added Water API

--- server/api/water.js
+++ server/api/water.js
@@ -3,14 +3,56 @@ const db = require('../db')
 
 const waterRouter = express.Router()
 
-function getWater() {
-    db.query({
-        sql: 'SELECT * FROM '
+function getWaterByUserID(id) {
+    return new Promise((resolve, reject) => {
+        db.query({
+            sql: 'SELECT * FROM userWater WHERE userID = ?',
+            values: [id]
+        }, function(err, row) {
+            if (err) reject(err);
+            else {
+                resolve(row)
+            }
+        })
+    })
+}
+
+function createWater(id, data) {
+    return new Promise((resolve, reject) => {
+        var yesterday = new Date()
+        yesterday.setDate(yesterday.getDate() - 1)
+        db.query({
+            sql: 'INSERT INTO userWater (waterQuantity, dateSubmitted, surveyID, userID) VALUES (?, ?, ?, ?)',
+            values: [data.waterQuantity, yesterday, 3, id]
+        }, function(err, row) {
+            if (err) reject(err);
+            else {
+                resolve(row)
+            }
+        })
     })
 }
 
 waterRouter.get('/', (req, res) => {
-    
+    getWaterByUserID(req.user.userID)
+    .then(res => {
+        res.json(res)
+    })
+    .catch(err => {
+        console.log(err)
+        res.json({err: err})
+    })
+})
+
+waterRouter.post('/', (res, res) => {
+    createWater(req.user.userID, req.body)
+    .then(result => {
+        res.json(result)
+    })
+    .catch(err => {
+        console.log(err)
+        res.json({err: err})
+    })
 })
 
 module.exports = waterRouter

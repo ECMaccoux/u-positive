fdc7fbecb86970f0e505fc016471986ee808ff2e
thok-patrol <rswerner@wisc.edu>
2020-09-19 20:05:34 -0500

    Create Depression API

--- server/api/api.js
+++ server/api/api.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const depressionRouter = require('./depression')
 const userRouter = require('./user')
 
 const apiRouter = express.Router()
 
 apiRouter.use('/user', userRouter)
+apiRouter.use('/depression', depressionRouter)
 
 apiRouter.get('/', (req, res) => {
     res.send('Welcome to the UPAPI (U-Positive Application Programming Interface). Please check back later for a better documented API and well rested programmers.')
---
+++ server/api/depression.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const db = require('../db')
+
+function getDepressionScoresByUserID(id) {
+    return new Promise((resolve, reject) => {
+        db.query({sql: 'SELECT * FROM userDepression WHERE userID = ?', values: [id]}, function(err, row) {
+            err ? reject(err) : resolve(row)
+        })
+    })
+}
+
+function setOrModifyDepressionScores(id, data) {
+    return new Promise((resolve, reject) => {
+        var today = new Date();
+    today.toISOString().slice(0, 19).replace("T", " ");
+    db.query({sql: 'SELECT * FROM userDepression WHERE userID = ? AND dateSubmitted = ?', values: [id, today]}, function(err, rows) {
+        if (err) reject(err);
+        else if (rows[0]) {
+            db.query({sql: 'UPDATE userDepression SET question1 = ?, question2 = ?, question3 = ? WHERE userID = ? AND dateSubmitted = ?', values: [data.question1, data.question2, data.question3, id, today]}, function(err, rows) {
+                if (err) reject(err);
+                else {
+                    resolve(rows)
+                }
+            })
+        } else {
+            db.query({sql: 'INSERT INTO userDepression (question1, question2, question3, dateSubmitted, userID, surveyID) VALUES (?, ?, ?, ?, ?)', values: [data.question1, data.question2, data.question3, id, today, 2]}, function(err, rows) {
+                if (err) reject(err);
+                else {
+                    resolve(rows)
+                }
+            })
+        }
+    })
+    })
+    
+}
+
+const depressionRouter = express.Router()
+
+depressionRouter.get('/', (req, res) => {
+    getDepressionScoresByUserID(req.user.userID)
+    .then(results => {
+        res.json(results)
+    })
+    .catch(err => {
+        console.log(err)
+        res.json({err: err}).status(500)
+    })
+})
+
+depressionRouter.post('/', (req, res) => {
+    const data = req.body;
+    setOrModifyDepressionScores(req.user.id, data)
+    .then(results => {
+        res.json(results).status(200)
+    })
+    .catch(err => {
+        console.log(err)
+        res.json({err: err}).status(500)
+    })
+})
+
+module.exports = depressionRouter
